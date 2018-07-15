d95c6a845d7e1625840608b5d301ef6ed251354d
Mikael <mikael@dwmedia.se>
2018-07-15 21:05:55 +0200

    the jump
    Jump was ../ at the /src but its 2 jump back but the src catalog is just one jump back.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        background: path.join(__dirname, '../src/scripts/background.ts'),
-        menu: path.join(__dirname, '../src/scripts/menu.ts'),
-        timeline: path.join(__dirname, '../src/scripts/timeline.ts')
+        background: path.join(__dirname, './src/scripts/background.ts'),
+        menu: path.join(__dirname, './src/scripts/menu.ts'),
+        timeline: path.join(__dirname, './src/scripts/timeline.ts')
     },
     output: {
-        path: path.join(__dirname, '../extension/scripts'),
+        path: path.join(__dirname, './extension/scripts'),
         filename: '[name].js'
     },
     resolve: {
